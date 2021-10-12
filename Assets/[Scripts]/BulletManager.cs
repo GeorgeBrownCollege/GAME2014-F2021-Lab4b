e53989bbe73b3379b4ad6892642b64e220149a86
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-10-12 16:26:35 -0400

    Added capability to Manage different Bullets

--- Assets/[Scripts]/BulletManager.cs
+++ Assets/[Scripts]/BulletManager.cs
@@ -6,22 +6,38 @@ using UnityEngine;
 [System.Serializable]
 public class BulletManager : MonoBehaviour
 {
-    public Queue<GameObject> bulletPool;
-    public int bulletNumber;
+    public Queue<GameObject> enemyBulletPool;
+    public Queue<GameObject> playerBulletPool;
+
+    public int enemyBulletNumber;
+    public int playerBulletNumber;
 
     private BulletFactory factory;
 
     // Start is called before the first frame update
     void Start()
     {
-        bulletPool = new Queue<GameObject>(); // creates an empty Queue
+        enemyBulletPool = new Queue<GameObject>(); // creates an empty Queue
+        playerBulletPool = new Queue<GameObject>(); // creates an empty Queue
+
         factory = GetComponent<BulletFactory>();
     }
 
-    private void AddBullet()
+    private void AddBullet(BulletType type = BulletType.ENEMY)
     {
-        var temp_bullet = factory.createBullet();
-        bulletPool.Enqueue(temp_bullet);
+        var temp_bullet = factory.createBullet(type);
+
+        switch (type)
+        {
+            case BulletType.ENEMY:
+                enemyBulletPool.Enqueue(temp_bullet);
+                enemyBulletNumber++;
+                break;
+            case BulletType.PLAYER:
+                playerBulletPool.Enqueue(temp_bullet);
+                playerBulletNumber++;
+                break;
+        }
     }
 
     /// <summary>
@@ -30,15 +46,30 @@ public class BulletManager : MonoBehaviour
     /// </summary>
     /// <param name="spawnPosition"></param>
     /// <returns></returns>
-    public GameObject GetBullet(Vector2 spawnPosition)
+    public GameObject GetBullet(Vector2 spawnPosition, BulletType type = BulletType.ENEMY)
     {
-        if (bulletPool.Count < 1)
+        GameObject temp_bullet = null;
+
+        switch (type)
         {
-            AddBullet();
-            bulletNumber++;
+            case BulletType.ENEMY:
+                if (enemyBulletPool.Count < 1)
+                {
+                    AddBullet(type);
+                }
+
+                temp_bullet = enemyBulletPool.Dequeue();
+                break;
+            case BulletType.PLAYER:
+                if (playerBulletPool.Count < 1)
+                {
+                    AddBullet(type);
+                }
+
+                temp_bullet = playerBulletPool.Dequeue();
+                break;
         }
 
-        var temp_bullet = bulletPool.Dequeue();
         temp_bullet.transform.position = spawnPosition;
         temp_bullet.SetActive(true);
         return temp_bullet;
@@ -48,9 +79,18 @@ public class BulletManager : MonoBehaviour
     /// This method returns a bullet back into the bullet pool
     /// </summary>
     /// <param name="returnedBullet"></param>
-    public void ReturnBullet(GameObject returnedBullet)
+    public void ReturnBullet(GameObject returnedBullet, BulletType type = BulletType.ENEMY)
     {
         returnedBullet.SetActive(false);
-        bulletPool.Enqueue(returnedBullet);
+
+        switch (type)
+        {
+            case BulletType.ENEMY:
+                enemyBulletPool.Enqueue(returnedBullet);
+                break;
+            case BulletType.PLAYER:
+                playerBulletPool.Enqueue(returnedBullet);
+                break;
+        }
     }
 }
